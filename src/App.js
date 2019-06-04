ea7552d8b3b13ddabf56fa5539d1cb06d55ea3f2
Kacper <kacper.olek@gmail.com>
2019-06-04 22:01:43 +0200

    homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Homepage from './Components/Homepage';
+import Skills from './Components/Skills';
+import Projects from './Components/Projects';
 
 const App = () => {
-    return <div></div>;
+    return (<div><Homepage/> <Skills/> <Projects/></div>);
 };
 
 export default App;
---
+++ src/Components/Icon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ProfilePicture = () => {
+    return <img className="profilePicture" src="./profile.jpg" alt="My face."></img>
+}
+
+export default ProfilePicture;
