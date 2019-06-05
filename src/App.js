dcc77d48c1bf1cef471ab4a7de6b5bf5a7851a9d
Kacper <kacper.olek@gmail.com>
2019-06-05 19:14:48 +0200

    Redesign

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
 import React from 'react';
-import Homepage from './Components/Homepage';
-import Skills from './Components/Skills';
-import Projects from './Components/Projects';
-import Contact from './Components/Contact';
 
 const App = () => {
-    return (<div><Homepage/> <Skills/> <Projects/> <Contact /></div>);
+    return <h1>Kacper</h1>
 };
 
 export default App;
--- src/Components/Icon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ProfilePicture = () => {
-    return <img className="profilePicture" src="./profile.jpg" alt="My face."></img>
-}
-
-export default ProfilePicture;
