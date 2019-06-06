9aa97dc3f70e736f05228bbdef9653132c1e5243
Kacper <kacper.olek@gmail.com>
2019-06-06 19:47:16 +0200

    styles changes

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,9 @@ import '../styles/footer.scss';
 
 
 const Footer = () => {
-    return (<footer><p>Get in touch on Github and LinkedIn</p></footer>);
+    return (<footer>
+        <p>Get in touch on <a target="_blank" rel="noopener noreferrer" href="https://github.com/Shavrin">Github</a> and <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/kacper-olek-987a518b/">LinkedIn</a></p>
+    </footer>);
 }
 
 export default Footer;
