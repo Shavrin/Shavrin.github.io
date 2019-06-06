7191371dfed5836cf3f6438ca57a9acef1e29d3f
Kacper <kacper.olek@gmail.com>
2019-06-06 23:05:56 +0200

    added about me section with animation

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import '../styles/header.scss';
 
-const Header = (props) => {
-    return (
-        <header>
-            <h1>Kacper Olek</h1>
-            <p className="aboutMe">Web Developer</p>
-            <div className={"line" + (props.animate ? " animate" : "")}></div>
-        </header>);
+class Header extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 'aboutExpanded': false };
+    };
+
+    toggleAbout() {
+        this.setState({ 'aboutExpanded': !this.state.aboutExpanded });
+    };
+
+    render() {
+        return (
+            <header>
+                <h1>Kacper Olek</h1>
+                <p>Web Developer</p>
+                <p className="aboutMe" onClick={this.toggleAbout.bind(this)}>About me</p>
+                <div className={"line" + (this.props.animate ? " animate" : "")}></div>
+                <div className={'aboutMeSection' + (this.state.aboutExpanded ? ' expanded' : "")}>
+                    <p>Hi, I'm Kacper!</p>
+                    <p>I'm a Web Developer!</p>
+                    <p>I'm an IT engineer!</p>
+                    <p>I love programming!</p>
+                </div>
+            </header >);
+    };
 };
 
 export default Header;
